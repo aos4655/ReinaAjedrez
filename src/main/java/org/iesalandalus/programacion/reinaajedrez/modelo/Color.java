22be9d55b322ccc82838d9dc63bc50af9580d0f1
antonio <antonio@DESKTOP-KGV1ICK>
2022-11-14 14:12:50 +0100

    CREADO ENUMERADO COLOR

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -1,5 +1,18 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public enum Color {
+	BLANCO("Blanco"),
+	NEGRO("Negro");
+	
+	private String cadenaAMostrar;
 
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+	
+	public String toString() {
+		return cadenaAMostrar;
+	}
+	
 }
+
