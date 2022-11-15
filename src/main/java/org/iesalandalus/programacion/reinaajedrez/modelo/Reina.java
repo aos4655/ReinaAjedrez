7c8a6451a5efe1f6c6e36008e89c4f42adb57b2d
antonio <antonio@DESKTOP-KGV1ICK.mshome.net>
2022-11-15 12:16:56 +0100

    CLASE REINA PASA TEST

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,7 +1,84 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Reina {
 	private Posicion posicion;
 	private Color color;
+	
+	public Reina() {
+		setColor(color.BLANCO);
+	}
+	public Reina(Color color) {
+		if(color == null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
+		}
+		setColor(color);
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		this.posicion = new Posicion(posicion);
+	}
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		if(color == color.BLANCO) {
+			this.posicion = new Posicion(1, 'd');
+		}
+		if(color == color.NEGRO) {
+			this.posicion = new Posicion(8, 'd');
+		}
+		this.color = color;
+	}
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException  {
+		if(direccion == null) {
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+		if(pasos >7 || pasos <1) {
+			throw new IllegalArgumentException("ERROR: El número de pasos debe estar comprendido entre 1 y 7.");
+		}
+		try {
+				switch (direccion) {
+				case NORTE: 
+					setPosicion(new Posicion(posicion.getFila()+pasos, posicion.getColumna()));
+					break;
+				case SUR: 
+					setPosicion(new Posicion(posicion.getFila()-pasos, posicion.getColumna()));
+					break;
+				case NORESTE: 
+					setPosicion(new Posicion(posicion.getFila()+pasos, (char)(posicion.getColumna()+pasos)));
+					break;
+				case NOROESTE: 
+					setPosicion(new Posicion(posicion.getFila()+pasos, (char)(posicion.getColumna()-pasos)));
+					break;
+				case ESTE: 
+					setPosicion(new Posicion(posicion.getFila(), (char)(posicion.getColumna()+pasos)));
+					break;
+				case SURESTE: 
+					setPosicion(new Posicion(posicion.getFila()-pasos, (char)(posicion.getColumna()+pasos)));
+					break;
+				case SUROESTE: 
+					setPosicion(new Posicion(posicion.getFila()-pasos, (char)(posicion.getColumna()-pasos)));
+					break;
+				case OESTE: 
+					setPosicion(new Posicion(posicion.getFila(), (char)(posicion.getColumna()-pasos)));
+					break;
+				}	
+			
+		}
+		catch (IllegalArgumentException e) {
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+		}
+		
+		
+	}
+	@Override
+	public String toString() {
+		return "color=" + color + ", posicion=(" + posicion+")";
+	}
+	
 	
 }
