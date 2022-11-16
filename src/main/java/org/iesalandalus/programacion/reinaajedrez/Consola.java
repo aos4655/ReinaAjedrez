7bfae31af030afff8aef2d1ecd07ef5f7678c116
antonio <antonio@DESKTOP-KGV1ICK.mshome.net>
2022-11-16 13:28:41 +0100

    CONSOLA Y MAIN FUNCIONAN BIEN

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -3,12 +3,13 @@ package org.iesalandalus.programacion.reinaajedrez;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
 
 import org.iesalandalus.programacion.reinaajedrez.modelo.Direccion;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class Consola {
 	private Consola() {
-		
 	}
-	public void MostrarMenu() {
+	public static void MostrarMenu() {
 		System.out.println("Las opciones de nuestro menu son:");
 		System.out.println("1) Crear reina por defecto");
 		System.out.println("2) Crear reina eligiendo color");
@@ -16,40 +17,95 @@ public class Consola {
 		System.out.println("4) Salir");
 
 	}
-	public int elegirOpcionMenu() {
-		int opcion=0;
-		switch (opcion) {
-			case 1: 
-				opcion =1;
-				break;
-			case 2:
-				opcion =2;
-				break;
-			case 3:
-				opcion =3;
-				break;
-			case 4:
-				opcion =4;
-				break;
+	public static int elegirOpcionMenu() {
+		int opcion = 0;
+		while (opcion<1 || opcion >4) {
+			System.out.println("Elija la opcion");
+			opcion = Entrada.entero();
 		}
+		
 		return opcion;	
 	}
-	public Color elegirColor() {
-		return null;
+	public static Color elegirColor() {
+		System.out.println("Elija un color:");
+		System.out.println("1) Blanco");
+		System.out.println("2) Negro");
+		Color color = null;
+		int c = Entrada.entero();
+		while (c<1 || c>2) {
+			System.out.println("Elija el color");
+			System.out.println("1) Blanco");
+			System.out.println("2) Negro");
+			c = Entrada.entero();
+		}
+		switch (c) {
+		case 1: 
+			color = Color.BLANCO;
+			break;
+		case 2:
+			color = Color.NEGRO;
+			break;
+		}
+		return color;
+		
 		
 	}
-	public void mostrarMenuDirecciones() {
-		
+	public static void mostrarMenuDirecciones() {
+		System.out.println("Las posibles direcciones son:");
+		System.out.println("1) Norte");
+		System.out.println("2) Sur");
+		System.out.println("3) Este");
+		System.out.println("4) Oeste");
+		System.out.println("5) Noreste");
+		System.out.println("6) Noroeste");
+		System.out.println("7) Sureste");
+		System.out.println("8) Suroeste");
 	}
-	public Direccion elegirDireccion() {
-		return null;
-		
+	public static Direccion elegirDireccion() {
+		System.out.println("Elija una direccion (numero): ");
+		Direccion direccion = null;
+		int d = Entrada.entero();
+		while (d<1 && d>8) {
+			System.out.println("Elija una direccion (numero): ");
+			d = Entrada.entero();	
+		}
+		switch (d) {
+		case 1: 
+			direccion = Direccion.NORTE;
+			break;
+		case 2:
+			direccion = Direccion.SUR;
+			break;
+		case 3: 
+			direccion = Direccion.ESTE;
+			break;
+		case 4:
+			direccion = Direccion.OESTE;
+			break;
+		case 5: 
+			direccion = Direccion.NORESTE;
+			break;
+		case 6:
+			direccion = Direccion.NOROESTE;
+			break;
+		case 7: 
+			direccion = Direccion.SURESTE;
+			break;
+		case 8:
+			direccion = Direccion.SUROESTE;
+			break;
+		}
+		return direccion;
 	}
-	public int elegirPasos() {
-		return 0;
-		
+	public static int elegirPasos() {
+		int pasos = 0;
+		while (pasos<1 || pasos >8) {
+			System.out.println("Introduzca el numero de pasos: ");
+			pasos = Entrada.entero();
+		}
+		return pasos;
 	}
-	public void desperdirse() {
+	public static void desperdirse() {
 		System.out.println("Hasta luego Lucas!");
 	}
 }
--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,8 +1,67 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Direccion;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
+
 public class MainApp {
+	private static Reina reina1;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws OperationNotSupportedException {
+		Consola.MostrarMenu();
+		int op= Consola.elegirOpcionMenu();
+		ejecutarOpcion(op);
 		
+		while(op != 4) {
+			Consola.MostrarMenu();
+			op= Consola.elegirOpcionMenu();
+			ejecutarOpcion(op);
+			
+		}
+		
+		Consola.desperdirse();
+	}
+	public static void ejecutarOpcion(int opcion) throws OperationNotSupportedException {
+		switch (opcion) {
+		case 1: 
+			crearReinaDefecto();
+			mostrarReina();
+			break;
+		case 2:
+			Color c = Consola.elegirColor();
+			crearReinaColor(c);
+			mostrarReina();
+			break;
+		case 3:
+			mover();
+			mostrarReina();
+			break;
+		case 4:
+			Consola.desperdirse();
+			break;
+		}
+	}
+	public static void crearReinaDefecto() {
+		reina1 = new Reina();
+	}
+	public static void crearReinaColor(Color color) {
+		reina1 = new Reina(color);
+	}
+	public static void mover() throws OperationNotSupportedException  {
+		if(reina1==null) {
+			System.out.println("ERROR: Debes crear primero una reina");
+		}
+		else {
+			Consola.mostrarMenuDirecciones();
+			Direccion d = Consola.elegirDireccion();
+			int p = Consola.elegirPasos();
+			reina1.mover(d, p);
+		}
+		
+	}
+	public static void mostrarReina() {
+		System.out.println(reina1.toString());
 	}
 }
